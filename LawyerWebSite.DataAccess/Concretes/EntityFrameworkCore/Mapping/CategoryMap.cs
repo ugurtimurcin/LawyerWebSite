6137b47029471f545fe1651d6b49a38ae82a6cad
Ugur Timurcin <utimurcin@outlook.com>
2021-02-27 12:45:03 +0300

    Default data added

--- LawyerWebSite.DataAccess/Concretes/EntityFrameworkCore/Mapping/CategoryMap.cs
+++ LawyerWebSite.DataAccess/Concretes/EntityFrameworkCore/Mapping/CategoryMap.cs
@@ -15,6 +15,12 @@ namespace LawyerWebSite.DataAccess.Concretes.EntityFrameworkCore.Mapping
             builder.Property(a => a.Name).IsRequired();
 
             builder.HasMany(a => a.Articles).WithOne(s => s.Category).HasForeignKey(a => a.CategoryId);
+
+            builder.HasData(
+                new Category { Id = 1, Name = "Ceza Hukuku", Url = "ceza-hukuku" },
+                new Category { Id = 2, Name = "Tazminat Hukuku", Url = "tazminat-hukuku" },
+                new Category { Id = 3, Name = "Medeni Hukuk", Url = "medeni-hukuk" }
+                );
         }
     }
 }
--- LawyerWebSite.DataAccess/Concretes/EntityFrameworkCore/Mapping/WorkAreaMap.cs
+++ LawyerWebSite.DataAccess/Concretes/EntityFrameworkCore/Mapping/WorkAreaMap.cs
@@ -8,11 +8,17 @@ using System.Text;
 
 namespace LawyerWebSite.DataAccess.Concretes.EntityFrameworkCore.Mapping
 {
-    public class WorkAreaMap : IEntityTypeConfiguration<WokrArea>
+    public class WorkAreaMap : IEntityTypeConfiguration<WorkArea>
     {
-        public void Configure(EntityTypeBuilder<WokrArea> builder)
+        public void Configure(EntityTypeBuilder<WorkArea> builder)
         {
             builder.Property(x => x.Desciption).HasColumnType("nvarchar(max)");
+
+            builder.HasData(
+                new WorkArea { Id = 1, CategoryId = 1, Desciption = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent aliquam sem non odio commodo consectetur. Curabitur sem nunc, maximus sed odio a, faucibus ullamcorper nisl. Fusce in neque turpis. Fusce eget sapien non nibh suscipit eleifend. Nullam enim tortor, laoreet in scelerisque ac, facilisis in nibh. Nulla facilisi. Donec ultricies porttitor rhoncus.", Picture = "ceza-hukuku.jpg" },
+                new WorkArea { Id = 2, CategoryId = 2, Desciption = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent aliquam sem non odio commodo consectetur. Curabitur sem nunc, maximus sed odio a, faucibus ullamcorper nisl. Fusce in neque turpis. Fusce eget sapien non nibh suscipit eleifend. Nullam enim tortor, laoreet in scelerisque ac, facilisis in nibh. Nulla facilisi. Donec ultricies porttitor rhoncus.", Picture = "tazminat-hukuku.jpg" },
+                new WorkArea { Id = 3, CategoryId = 3, Desciption = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent aliquam sem non odio commodo consectetur. Curabitur sem nunc, maximus sed odio a, faucibus ullamcorper nisl. Fusce in neque turpis. Fusce eget sapien non nibh suscipit eleifend. Nullam enim tortor, laoreet in scelerisque ac, facilisis in nibh. Nulla facilisi. Donec ultricies porttitor rhoncus.", Picture = "medeni-hukuk.jpg" }
+                );
         }
     }
 }
