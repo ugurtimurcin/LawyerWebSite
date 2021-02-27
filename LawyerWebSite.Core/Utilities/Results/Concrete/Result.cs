8cacf8c5a6c0af8da9bbcda2e9acd113edc08ace
Ugur Timurcin <utimurcin@outlook.com>
2021-02-27 16:55:09 +0300

    Result utility builded

---
+++ LawyerWebSite.Core/Utilities/Results/Concrete/Result.cs
@@ -0,0 +1,25 @@
+﻿using LawyerWebSite.Core.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawyerWebSite.Core.Utilities.Results.Concrete
+{
+    public class Result : IResult
+    {
+        public Result(bool success, string message) : this(success)
+        {
+            Message = message;
+        }
+        public Result(bool success)
+        {
+            Success = success;
+        }
+        public string Message { get; }
+
+        public bool Success { get; }
+
+    }
+}
