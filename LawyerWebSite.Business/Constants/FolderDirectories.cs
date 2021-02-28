8a9ed5de9ff60cf66dba3caa3380fccd1ebf57f2
Ugur Timurcin <utimurcin@outlook.com>
2021-02-28 23:04:49 +0300

    Class created for keeping folder directories.

---
+++ LawyerWebSite.Business/Constants/FolderDirectories.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawyerWebSite.Business.Constants
+{
+    public static class  FolderDirectories
+    {
+        public static string ArticleFolder = "article-cover";
+        public static string WorkareaFolder = "workarea-cover";
+    }
+}
