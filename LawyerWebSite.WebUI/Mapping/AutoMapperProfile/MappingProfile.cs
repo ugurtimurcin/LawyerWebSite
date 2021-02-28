b68733ec04915c072fbfb87c00c334a753e9eb36
Ugur Timurcin <utimurcin@outlook.com>
2021-02-28 23:08:04 +0300

    AutoMapper profil created.

---
+++ LawyerWebSite.WebUI/Mapping/AutoMapperProfile/MappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using LawyerWebSite.Entities.Concrete.DTOs;
+using LawyerWebSite.Entities.Concrete.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LawyerWebSite.WebUI.Mapping.AutoMapperProfile
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<AppUserEditDto, AppUser>().ReverseMap();
+            CreateMap<AppUserListDto, AppUser>().ReverseMap();
+            CreateMap<AppUserLoginDto, AppUser>().ReverseMap();
+            CreateMap<AppUserRegisterDto, AppUser>().ReverseMap();
+            CreateMap<AppUserUpdatePasswordDto, AppUser>().ReverseMap();
+            CreateMap<ArticleAddDto, Article>().ReverseMap();
+            CreateMap<ArticleEditDto, Article>().ReverseMap();
+            CreateMap<ArticleListDto, Article>().ReverseMap();
+            CreateMap<CategoryAddDto, Category>().ReverseMap();
+            CreateMap<CategoryAllListDto, Category>().ReverseMap();
+            CreateMap<CategoryEditDto, Category>().ReverseMap();
+            CreateMap<CategoryListDto, Category>().ReverseMap();
+            CreateMap<WorkAreaAddDto, WorkArea>().ReverseMap();
+            CreateMap<WorkAreaEditDto, WorkArea>().ReverseMap();
+            CreateMap<WorkAreaListDto, WorkArea>().ReverseMap();
+        }
+    }
+}
