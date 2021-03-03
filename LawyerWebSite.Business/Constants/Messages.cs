9e04b1d1caa0fa49349dba19cab5b282396dffb6
Ugur Timurcin <utimurcin@outlook.com>
2021-03-03 20:42:23 +0300

    Const message class created

---
+++ LawyerWebSite.Business/Constants/Messages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawyerWebSite.Business.Constants
+{
+    public static class Messages
+    {
+        public static string CategoryAlreadyExists = "Oluşturmak istediğiniz kategori sistemde kayıtlı";
+    }
+}
